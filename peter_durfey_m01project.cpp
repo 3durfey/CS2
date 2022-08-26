1a17acba5854800b89d6e67c3410e2e8c88a7436
ark111 <56773327+3durfey@users.noreply.github.com>
2022-08-25 23:25:49 -0400

    commit

---
+++ peter_durfey_m01project.cpp
@@ -0,0 +1,67 @@
+/* Program name: peter_durfey_m01project.cpp
+*  Author: Peter Durfey
+*  Date last updated: 8/25/2022
+* Purpose: basic function
+*/
+
+#include <iostream>
+using namespace std;
+
+class Cube 
+{
+    int length, width, height;
+    string color;
+
+    Cube(int x, int y, int q, string f) 
+    {
+        length = x;
+        width = y;
+        height = q;
+        color = f;
+    }
+    public:
+    //accessor
+    int getLength()
+    {
+        return length;
+    }
+    int getWidth()
+    {
+        return width;
+    }
+    int getHeight()
+    {
+        return height;
+    }
+    string getColor()
+    {
+        return color;
+    }
+    //mutators
+    void setLength(int l)
+    {
+        length = l; 
+    }
+    void setHeight(int h)
+    {
+        height = h;
+    }
+    void setColor(string c)
+    {
+        color = c;
+    }
+    //volume= length*height
+    int calculateVolume(int l, int h, int c)
+    {
+        return (l * h * c);
+    }
+
+};
+
+int main() 
+{
+    
+}
+
+
+
--- readme.txt
+++ readme.txt
@@ -1,4 +1,6 @@
-Write a short program which has a function that is called with a number and a string.  Within the function do these things in this order:
+Write a short program which has a function that is 
+called with a number and a string.  
+Within the function do these things in this order:
 
 1. output the passed values
 
